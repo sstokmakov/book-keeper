3afa73dea80ad9565382af8d785eab0428ab34cf
Yaroslav Tokmakov <ytokmakoff@gmail.com>
2024-11-22 12:25:57 +0300

    first commit

---
+++ src/main/java/ru/tokmakov/bookkeeper/BookKeeperApplication.java
@@ -0,0 +1,13 @@
+package ru.tokmakov.bookkeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookKeeperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookKeeperApplication.class, args);
+    }
+
+}
